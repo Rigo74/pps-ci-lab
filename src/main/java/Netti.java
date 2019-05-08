1bc1ad82e82f783d71042316b34b5120d4f30775
Francesco Grandinetti <francesco.grandinett@gmail.com>
2019-05-08 14:40:19 +0200

    add javadoc and sum function

--- src/main/java/Netti.java
+++ src/main/java/Netti.java
@@ -1,7 +1,24 @@
+/**
+ * My class.
+ * @author Francesco Grandinetti
+ */
 public class Netti {
-
+    /**
+     * Entry point
+     * @param args arguments
+     */
     public static void main(String[] args) {
         System.out.println("Hello There by Netti");
+    }
+
+    /**
+     * Performs the sum between two numbers
+     * @param a the first number
+     * @param b the second number
+     * @return the sum
+     */
+    public static int sum(final int a, final int b){
+        return a + b;
     }
 
 }
