82993e1bc93bf90dc7c47195ecd702225469ae97
Stefano Righini <stefano.righini6@studio.unibo.it>
2019-05-08 14:46:58 +0200

    Add java doc for entry point.

--- src/main/java/StefanoRighini.java
+++ src/main/java/StefanoRighini.java
@@ -1,4 +1,9 @@
 public class StefanoRighini {
+
+    /**
+     * Entry point.
+     * @param args arguments
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
